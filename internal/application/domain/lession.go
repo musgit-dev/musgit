432f66b4f751fafcd7b1b2dd43af2d55335bd196
Andrei Markin <andrey.markin.ppc@gmail.com>
2025-06-11 00:04:10 +0400

    Add Lesson domain object

---
+++ internal/application/domain/lession.go
@@ -0,0 +1,47 @@
+package domain
+
+import "time"
+
+type LessonState int
+
+const (
+	LessonScheduled LessonState = iota
+	LessonActive
+	LessonPaused
+	LessonCompleted
+)
+
+type Lesson struct {
+	ID        int64
+	State     LessonState
+	StartDate time.Time
+	EndDate   time.Time
+	Comment   string
+}
+
+func NewLesson() *Lesson {
+	lesson := Lesson{StartDate: time.Now()}
+	lesson.State = LessonActive
+	return &lesson
+}
+
+func (l *Lesson) Pause() *Lesson {
+	l.State = LessonPaused
+	return l
+}
+
+func (l *Lesson) Resume() *Lesson {
+	l.State = LessonActive
+	return l
+}
+
+func (l *Lesson) Finish() *Lesson {
+	l.EndDate = time.Now()
+	l.State = LessonCompleted
+	return l
+}
+
+func (l *Lesson) AddNote(comment string) *Lesson {
+	l.Comment = comment
+	return l
+}
---
+++ internal/lesson_test.go
@@ -0,0 +1,47 @@
+package domain
+
+import (
+	"musgit/internal/application/domain"
+	"testing"
+)
+
+func TestLesson(t *testing.T) {
+
+	lesson := domain.NewLesson()
+
+	if lesson.StartDate.IsZero() {
+		t.Errorf("lesson not started")
+	}
+
+	if lesson.State != domain.LessonActive {
+		t.Errorf("Expected lesson to be Active")
+	}
+	if !lesson.EndDate.IsZero() {
+		t.Errorf("Lesson has non zero end date")
+	}
+
+	lesson.Pause()
+	if lesson.State != domain.LessonPaused {
+		t.Errorf("Expected lesson to be Paused")
+	}
+
+	lesson.Resume()
+	if lesson.State != domain.LessonActive {
+		t.Errorf("Expected lesson to be Active again")
+	}
+
+	lesson.Finish()
+	if lesson.EndDate.IsZero() {
+		t.Errorf("Lesson not completed")
+	}
+	if lesson.State != domain.LessonCompleted {
+		t.Errorf("Expected lesson to be Completed")
+	}
+
+	lesson.AddNote("Good")
+
+	if lesson.Comment != "Good" {
+		t.Errorf("Comment has not been added to the lesson.")
+	}
+
+}
