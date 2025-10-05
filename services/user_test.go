5fe85e0709a28b919e35d609d1b03a5894914b6c
Andrei Markin <andrey.markin.ppc@gmail.com>
2025-10-05 23:56:16 +0400

    tests: add tests for missing user service

--- services/user_test.go
+++ services/user_test.go
@@ -6,7 +6,7 @@ import (
 
 var userService *UserService
 
-func TestAddUser(t *testing.T) {
+func TestUserAdd(t *testing.T) {
 	user, err := userService.Add("test_user")
 	if err != nil {
 		t.Fatal(err)
@@ -14,9 +14,29 @@ func TestAddUser(t *testing.T) {
 	if user.Name != "test_user" {
 		t.Fatalf("Unexpected user name, got %s, expected test user", user.Name)
 	}
+	if userId := user.ID; userId != 1 {
+		t.Fatalf("Unexpected user id, got %d, expected test user", userId)
+	}
+
+	newUser, err := userService.Get(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if newUser.Name != user.Name {
+		t.Fatal("unexpected user")
+	}
+	users, err := userService.GetAll()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if nUsers := len(users); nUsers != 1 {
+		t.Fatalf("Unexpected number of users, expected 1, got %d", nUsers)
+	}
+
 }
 
-func TestAssignPiece(t *testing.T) {
+func TestUserAssignPiece(t *testing.T) {
 	user, err := userService.Add("test_user")
 	if err != nil {
 		t.Fatal(err)
@@ -28,6 +48,25 @@ func TestAssignPiece(t *testing.T) {
 	user.AssignPiece(&piece)
 	if len(user.Pieces) != 1 {
 		t.Fatalf("Piece wasn't added")
+	}
+}
+
+func TestUserStartPractice(t *testing.T) {
+	user, _ := userService.Add("test_user")
+	if nPractices := len(user.Practices); nPractices != 0 {
+		t.Fatalf(
+			"Unexpected number of practices, got %d, expected 0",
+			nPractices,
+		)
+	}
+	practice, err := userService.StartPractice(user.ID, 1, 1)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !practice.Active() {
+		t.Fatal("Practice is not active")
 	}
 
 }
